4d9fbcdad5b3f6008b0071c2fa3ca42e534c17e1
AI796 <129879574+AI796@users.noreply.github.com>
2023-09-04 17:36:42 +0800

    Update model.py

--- Streaming/1/model.py
+++ Streaming/1/model.py
@@ -8,27 +8,28 @@ import ffmpeg
 def streaming(images_batch,audio,streaming_server_url):
     fps=25
     _,height,width,_=images_batch.shape # 256,256
+    
+    # 下面ffmpeg代码未经测试，本人属于ffmpeg小白，可能有bug！音频也没有合并进来，求大佬提供样例
     process=(
         ffmpeg
         .input('pipe:',format='rawvideo',pix_fmt='bgr24',s='{}x{}'.format(width,height))
         .output(
-            vcodec='libx264', # [libx264,rawvideo,h264_nvenc]只有rawvideo才能随便设置ts长度，其他压缩格式各自有自己的最小长度，比如h264_nvenc最小10秒
-            # preset="fast", # h264_nvenc.fast代表了一系列参数组合
+            vcodec='libx264', # [libx264,rawvideo,h264_nvenc]需要进一步测试，好像只有rawvideo才能随便设置ts长度，其他压缩格式各自有自己的最小长度，比如h264_nvenc最小10秒
             format='segment', 
             pix_fmt='rgb24', 
-            segment_list=streaming_server_url+"/Playlist.m3u8", # 创建播放列表
+            segment_list=streaming_server_url+"/Playlist.m3u8", # 创建streaming server播放列表
             segment_time=1, # 每秒创建一个ts片段
             r=fps, # frame_rate fps
             start_number=0, # 需要在实际项目中改为你插入的点
             force_key_frames='expr:gte(t,n_forced*1)', # 强制每秒插入1个i帧，这句不知道对不对
-            filename=streaming_server_url+"/stream_%03d.ts" # 创建ts片段
+            filename=streaming_server_url+"/stream_%03d.ts" # 创建ts片段，发送到streaming server
             )
         .overwrite_output() 
         .run_async(pipe_stdin=True) 
         )
+    # 将25帧视频numpy数据顺序写入ffmpeg pipe
     for index in range(len(images_batch)): # len(images_batch)=25
         img=images_batch[index] # [256,256,3]
-        # 写入ffmpeg pipe
         process.stdin.write(
             img # bgr24
             .astype(np.uint8)
